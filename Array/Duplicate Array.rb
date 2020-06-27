069bbef477ef64a4dc648e62b4ae5fb91be0e7b4
chenerychen <60025491+chenerychen@users.noreply.github.com>
2020-06-27 16:50:45 -0700

    Update and rename Duplicate Array to Duplicate Array.rb

---
+++ Array/Duplicate Array.rb
@@ -0,0 +1,23 @@
+arr = Array.new(5, " ")
+# -> [" ", " ", " ", " ", " "]
+
+shortcut strings to array
+
+arr = %w(cat dog mouse 1 2)
+# -> ["cat", "dog", "mouse", "1", "2"]
+
+
+
+arr = Array.new(5, " ")
+# -> [" ", " ", " ", " ", " "]
+
+shortcut strings to array
+
+arr = %w(cat dog mouse 1 2)
+# -> ["cat", "dog", "mouse", "1", "2"]
+
+
+
+
+
+
